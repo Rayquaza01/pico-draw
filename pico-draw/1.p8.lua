b291b58b64b465a88a00facb63e153c6d68d4e75
Joe Jarvis <joe.jarvis109@gmail.com>
2023-08-06 18:49:14 -0400

    Add circle tool

--- pico-draw/1.p8.lua
+++ pico-draw/1.p8.lua
@@ -1,7 +1,7 @@
 --  drawing screen
 
 function init_drawing()
-	mode = make_cursor(4)
+	mode = make_cursor(5)
 
 	-- line drawing
 	line_start_x = nil
@@ -12,10 +12,17 @@ function init_drawing()
 	fill_start_x = nil
 	fill_start_y = nil
 
+	-- circle tool
+	circle_center_x = nil
+	circle_center_y = nil
+
 	dx_val = {0, 1, 0, -1}
 	dy_val = {-1, 0, 1, 0}
 
 	draw_speed = 5
+	draw_speed_line = 1
+	draw_speed_circle = 1
+	draw_speed_fill = 5
 
 	coroutines = {}
 
@@ -38,7 +45,7 @@ function update_drawing()
 	-- if left click or 🅾️
 	-- set color on field
 	if lmb and mode.selected == 0 and in_bounds then
-		field[m_x][m_y] = cur_color.selected
+		set_field(m_x, m_y, cur_color.selected)
 	end
 
 	if lmbp and in_bounds then
@@ -47,12 +54,12 @@ function update_drawing()
 				line_start_x = m_x
 				line_start_y = m_y
 
-				printh("set start to " .. line_start_x .. ", " .. line_start_y)
+				printh("set line start to " .. line_start_x .. ", " .. line_start_y)
 			else
 				local c = cocreate(draw_line)
-				coresume(c, line_start_x, line_start_y, m_x, m_y)
+				coresume(c, line_start_x, line_start_y, m_x, m_y, cur_color.selected)
 				add(coroutines, c)
-				printh("set end to " .. m_x .. ", " .. m_y)
+				printh("set line end to " .. m_x .. ", " .. m_y)
 
 				line_start_x = nil
 				line_start_y = nil
@@ -63,10 +70,29 @@ function update_drawing()
 			-- it wouldn't have any effect, and also causes infinite recursion
 			if not (replace_color == cur_color.selected) then
 				local fill_cor = cocreate(flood_fill)
-				coresume(fill_cor, m_x, m_y, replace_color)
+				coresume(fill_cor, m_x, m_y, replace_color, cur_color.selected)
 				add(coroutines, fill_cor)
 			end
 		elseif mode.selected == 3 then
+			if (circle_center_x == nil) then
+				circle_center_x = m_x
+				circle_center_y = m_y
+
+				printh("set circle center to " .. circle_center_x .. ", " .. circle_center_y)
+			else
+				-- get radius of circle using distance formula
+				-- sqrt((x - x0) ^ 2 + (y - y0) ^ 2))
+				radius = flr(sqrt((m_x - circle_center_x) ^ 2 + (m_y - circle_center_y) ^ 2))
+
+				local c = cocreate(draw_circle)
+				coresume(c, circle_center_x, circle_center_y, radius, cur_color.selected)
+				add(coroutines, c)
+				printh("set circle radius to " .. radius)
+
+				circle_center_x = nil
+				circle_center_y = nil
+			end
+		elseif mode.selected == 4 then
 			cur_color.selected = field[m_x][m_y]
 			mode.selected = 0
 		end
@@ -103,7 +129,7 @@ function draw_drawing()
 	end
 
 	-- draw mouse cursor
-	spr(4, m_x, m_y)
+	spr(1 + mode.selected, m_x, m_y)
 	-- draw current color indicator
 	pset(m_x, m_y, cur_color.selected)
 
@@ -111,6 +137,12 @@ function draw_drawing()
 		print(m_x .. ", " .. m_y, 0, 0)
 		print("mode = " .. mode.selected, 0, 8)
 		print("color = " .. cur_color.selected, 0, 16)
+	end
+end
+
+function set_field(x, y, val)
+	if is_inbounds(x, y) then
+		field[x][y] = val
 	end
 end
 
@@ -144,7 +176,7 @@ end
 -- implementation of bresenham's line algorithm
 -- https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 
-function plot_line_low(x0, y0, x1, y1)
+function plot_line_low(x0, y0, x1, y1, line_color)
     local dx = x1 - x0
     local dy = y1 - y0
     local yi = 1
@@ -156,7 +188,7 @@ function plot_line_low(x0, y0, x1, y1)
     local y = y0
 
     for x = x0, x1, 1 do
-		field[x][y] = cur_color.selected
+		set_field(x, y, line_color)
         if D > 0 then
             y = y + yi
             D = D + 2 * (dy - dx)
@@ -173,7 +205,7 @@ function plot_line_low(x0, y0, x1, y1)
 	end
 end
 
-function plot_line_high(x0, y0, x1, y1)
+function plot_line_high(x0, y0, x1, y1, line_color)
     local dx = x1 - x0
     local dy = y1 - y0
     local xi = 1
@@ -185,7 +217,7 @@ function plot_line_high(x0, y0, x1, y1)
     local x = x0
 
     for y = y0, y1, 1 do
-		field[x][y] = cur_color.selected
+		set_field(x, y, line_color)
         if D > 0 then
             x = x + xi
             D = D + 2 * (dx - dy)
@@ -202,20 +234,20 @@ function plot_line_high(x0, y0, x1, y1)
 	end
 end
 
-function draw_line(x0, y0, x1, y1)
-	line_speed = make_countdown(draw_speed)
+function draw_line(x0, y0, x1, y1, line_color)
+	line_speed = make_countdown(draw_speed_line)
 
 	if abs(y1 - y0) < abs(x1 - x0) then
         if x0 > x1 then
-            plot_line_low(x1, y1, x0, y0)
+            plot_line_low(x1, y1, x0, y0, line_color)
         else
-            plot_line_low(x0, y0, x1, y1)
+            plot_line_low(x0, y0, x1, y1, line_color)
         end
     else
         if y0 > y1 then
-            plot_line_high(x1, y1, x0, y0)
+            plot_line_high(x1, y1, x0, y0, line_color)
         else
-            plot_line_high(x0, y0, x1, y1)
+            plot_line_high(x0, y0, x1, y1, line_color)
         end
     end
 end
@@ -224,8 +256,8 @@ end
 -- https://en.wikipedia.org/wiki/Flood_fill
 -- adapted from https://github.com/Rayquaza01/minesweeper/blob/1edfc1f061032966f668cb0baf0b1f6ec6f63197/minesweeper/3.p8.lua#L112
 
-function flood_fill(x, y, replace_color)
-	local fill_speed = make_countdown(draw_speed)
+function flood_fill(x, y, replace_color, fill_color)
+	local fill_speed = make_countdown(draw_speed_fill)
 
 	pos = {}
 	pos.x = x
@@ -245,7 +277,7 @@ function flood_fill(x, y, replace_color)
 					add(stack, adj)
 				end
 			end
-			field[top.x][top.y] = cur_color.selected
+			set_field(top.x, top.y, fill_color)
 
 			if fill_speed.is_finished() then
 				fill_speed.reset()
@@ -256,4 +288,50 @@ function flood_fill(x, y, replace_color)
 		end
 	end
 
+end
+
+-- bresenham's circle algorithm
+-- based on https://www.geeksforgeeks.org/bresenhams-circle-drawing-algorithm/#
+
+-- xc and yc are the midpoint
+-- x and y are the distance in each direction to draw
+-- will draw all 8 octants
+function draw_circle_points(xc, yc, x, y, color)
+	set_field(xc + x, yc + y, color)
+	set_field(xc - x, yc + y, color)
+	set_field(xc + x, yc - y, color)
+	set_field(xc - x, yc - y, color)
+	set_field(xc + y, yc + x, color)
+	set_field(xc - y, yc + x, color)
+	set_field(xc + y, yc - x, color)
+	set_field(xc - y, yc - x, color)
+end
+
+function draw_circle(xc, yc, r, color)
+	local circle_speed = make_countdown(draw_speed_circle)
+
+	local x = 0
+	local y = r
+	local d = 3 - 2 * r
+
+	draw_circle_points(xc, yc, x, y, color)
+	circle_speed.subtract(1)
+
+	while y >= x do
+		x += 1
+
+		if d > 0 then
+			y -= 1
+			d = d + 4 * (x - y) + 10
+		else
+			d = d + 4 * x + 6
+		end
+
+		draw_circle_points(xc, yc, x, y, color)
+		circle_speed.subtract(1)
+
+		if circle_speed.is_finished() then
+			yield()
+		end
+	end
 end
