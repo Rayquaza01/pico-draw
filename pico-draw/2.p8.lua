acd5905c8bdc86aa1cddd1ed82a58a49d01beb07
Joe Jarvis <joe.jarvis109@gmail.com>
2023-08-14 20:29:38 -0400

    Update credits

--- pico-draw/2.p8.lua
+++ pico-draw/2.p8.lua
@@ -1,5 +1,5 @@
 function update_menu()
-    if btnp(⬅️) then
+    if btnp(⬇️) then
         gamestate = 0
     end
 
@@ -12,13 +12,17 @@ function update_menu()
         end
 
         if m_cell_y == 6 then
-            cur_color.selected = m_cell_x
-            gamestate = 0
+            if m_cell_x >= 0 and m_cell_x < 8 then
+                cur_color.selected = m_cell_x
+                gamestate = 0
+            end
         end
 
         if m_cell_y == 7 then
-            cur_color.selected = m_cell_x + 8
-            gamestate = 0
+            if m_cell_x >= 0 and m_cell_x < 8 then
+                cur_color.selected = m_cell_x + 8
+                gamestate = 0
+            end
         end
 
         if m_cell_x == 7 and m_cell_y == 0 then
@@ -42,4 +46,9 @@ function draw_credits()
     map(9, 0)
     print("lowrezjam 2023", 1, 7, 0)
     print("♥", 1 + 4 * 14, 7, 8)
+
+    print("pico-draw v1.0", 1, 12 + 1 * 8, 0)
+    print("by rayquaza01", 1, 12 + 2 * 8, 3)
+
+    print("⬇️ to close", 1, 12 + 5 * 8, 0)
 end
